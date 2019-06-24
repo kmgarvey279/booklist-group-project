acaaf2d5e75ec3d87e07e4d7faeaf8ff5b47299b
Guest User <Guest@epicodus-79.local>
2019-06-24 15:19:02 -0700

    Add current user display

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  user: firebase.User;
   public isCollapsed: boolean;
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => this.user = user);
+   }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
