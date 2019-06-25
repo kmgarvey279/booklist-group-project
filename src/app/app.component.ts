02fee1dd01bb4b0e79d1f1276e9b7a7a01c4bd2e
ScoutPilgrim <ianchristner@gmail.com>
2019-06-25 10:51:02 -0700

    All API calls up and running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GoodReadsService } from './good-reads.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GoodReadsService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  constructor(private googleBooks: GoodReadsService){
+
+  }
+  ngOnInit(){
+    this.googleBooks.searchBookByTitle('test').subscribe(val => console.log(val));
+    this.googleBooks.searchBookByAuthor('Charles Dickens').subscribe(val => console.log(val));
+    this.googleBooks.searchBookBySubject('Juvenile Fiction').subscribe(val => console.log(val));
+    this.googleBooks.searchBookByGeneral('test').subscribe(val => console.log(val));
+  }
 }
