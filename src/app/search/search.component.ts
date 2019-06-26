9c7a258267960ff9b8a14aecb9a3e68998906dba
Student <example@example.com>
2019-06-26 09:55:11 -0700

    Added search method to search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../book.service';
+import {GoodReadsService} from '../good-reads.service';
 
 @Component({
   selector: 'search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
-  providers: [BookService]
+  providers: [BookService, GoodReadsService]
 })
 export class SearchComponent implements OnInit {
 
-  constructor(private gr: BookService) { }
+  constructor(private googleBooks: GoodReadsService) { }
 
   ngOnInit() {
-    this.gr.getBooksByAuthor('175417', 1);
   }
-  search(title, author) {
-    
+  search(query:string, type:string) {
+    console.log(`Searching for ${query} & ${type}`);
+    switch (type) {
+      case "author":
+        this.googleBooks.searchBookByAuthor(query);
+        break;
+      case "title":
+        this.googleBooks.searchBookByTitle(query);
+        break;
+      case "subject":
+        this.googleBooks.searchBookBySubject(query);
+        break;
+      default:
+        this.googleBooks.searchBookByGeneral(query);
+    }
   }
 
 }
