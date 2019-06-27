a165060942515cac9450decc3f6d9442c90e9a59
ScoutPilgrim <ianchristner@gmail.com>
2019-06-27 13:08:52 -0700

    Got DB working

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -8,21 +8,27 @@ import { ActivatedRoute } from '@angular/router';
 export class AuthService {
   user$: Observable<firebase.User>;
   appUser$: any;
+  private user;
 
   constructor(
     private afAuth: AngularFireAuth,
     private route: ActivatedRoute) {
-    this.user$ = afAuth.authState;    
+    this.user$ = afAuth.authState;
+    afAuth.authState.subscribe(user => {
+      this.user = user.uid;
+    });
    }
-
    login() {
     let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
     localStorage.setItem('returnUrl', returnUrl);
-    
+    localStorage.setItem('user', this.user);
+
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
   }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.afAuth.auth.signOut().then(() => {
+      localStorage.removeItem('user');
+    });
   }
 }
--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Book } from './book.model';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class BookService {
   books: FirebaseListObservable<any[]>;
-
-  constructor(private af: AngularFireDatabase) {
-    this.books = af.list('books');
+  ref = null;
+  uid = null;
+  scopeFire = firebase;
+  constructor(private af: AngularFireDatabase, public fAuth: AngularFireAuth, public auth: AuthService) {
+    this.uid = localStorage.getItem('user');
+    this.books = af.list(`users/${this.uid}/books`);
   }
 
   addBook(newBook: Book) {
@@ -29,6 +35,6 @@ export class BookService {
   }
 
   getBookById(bookId: string) {
-    return this.af.object('books/' + bookId);
+    return this.af.object(`users/${this.uid}/books/${bookId}`);
   }
 }
